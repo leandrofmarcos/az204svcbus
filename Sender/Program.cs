6c39f310f909c924f10be9322ddd1dd7854480ad
Leandro Marcos <leandrofmarcos@gmail.com>
2022-12-13 18:12:11 -0300

    first commit

---
+++ Sender/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+public class Program{
+
+    static string connectionString = "";
+    static string queueName = "az204-queue";
+    static ServiceBusClient client;
+    static ServiceBusSender sender;
+    private const int numOfMessages = 3;
+
+    static async Task Main()
+    {
+        // Create the clients that we'll use for sending and processing messages.
+        client = new ServiceBusClient(connectionString);
+        sender = client.CreateSender(queueName);
+
+        // create a batch
+        using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+        for (int i = 1; i <= 3; i++)
+        {
+            // try adding a message to the batch
+            if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+            {
+                // if an exception occurs
+                throw new Exception($"Exception {i} has occurred.");
+            }
+        }
+
+        try
+        {
+            // Use the producer client to send the batch of messages to the Service Bus queue
+            await sender.SendMessagesAsync(messageBatch);
+            Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+        }
+        finally
+        {
+            // Calling DisposeAsync on client types is required to ensure that network
+            // resources and other unmanaged objects are properly cleaned up.
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
+        Console.WriteLine("Press any key to end the application");
+        Console.ReadKey();
+    }
+}
